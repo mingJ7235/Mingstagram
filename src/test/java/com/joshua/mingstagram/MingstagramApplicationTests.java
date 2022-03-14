e1c4cc231454c1e4701efaf93d5cf4383ca56e25
김민재 <joshuara7235@gmail.com>
2022-03-14 21:57:52 +0900

    Git init commit

---
+++ src/test/java/com/joshua/mingstagram/MingstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshua.mingstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
