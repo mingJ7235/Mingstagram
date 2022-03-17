3846d751935ccd0bb8808a699ec44283ff529f41
김민재 <joshuara7235@gmail.com>
2022-03-17 23:10:25 +0900

    1. UserController made

---
+++ src/main/java/com/joshua/mingstagram/domain/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.joshua.mingstagram.domain.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class UserController {
+
+
+}
