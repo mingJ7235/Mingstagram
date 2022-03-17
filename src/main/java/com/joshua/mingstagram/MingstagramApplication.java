4da251764531d5f28fff097a1685404dd80a5439
김민재 <joshuara7235@gmail.com>
2022-03-17 23:22:44 +0900

    1. Security encoder Bean 순환참조 방지 위해 Application 에 Bean 주입 (다른 방법 찾아야함)
    2. UserController 테스트용으로 핸들러 추가

--- src/main/java/com/joshua/mingstagram/MingstagramApplication.java
+++ src/main/java/com/joshua/mingstagram/MingstagramApplication.java
@@ -2,9 +2,16 @@ package com.joshua.mingstagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MingstagramApplication {
+
+    @Bean
+    public BCryptPasswordEncoder encoder () {
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MingstagramApplication.class, args);
--- src/main/java/com/joshua/mingstagram/domain/controller/UserController.java
+++ src/main/java/com/joshua/mingstagram/domain/controller/UserController.java
@@ -3,11 +3,20 @@ package com.joshua.mingstagram.domain.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class UserController {
 
+    @GetMapping ("/auth/login")
+    public String login () {
+        return "auth/login";
+    }
 
+    @GetMapping ("/auth/kakao-login")
+    public String kakaoLogin () {
+        return "auth/kakaoLogin";
+    }
 }
