5f4898214179ca6937cbb4d912a5f8f3d2093705
김민재 <joshuara7235@gmail.com>
2022-03-22 21:16:10 +0900

    1. Thymeleaf sec 설정 확인
    2. value 불러 오는지 controller 에서 확인

--- src/main/java/com/joshua/mingstagram/domain/controller/ImageController.java
+++ src/main/java/com/joshua/mingstagram/domain/controller/ImageController.java
@@ -1,6 +1,9 @@
 package com.joshua.mingstagram.domain.controller;
 
+import com.joshua.mingstagram.domain.model.User;
+import com.joshua.mingstagram.domain.repository.UserRepository;
 import com.joshua.mingstagram.global.auth.MyUserDetail;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -9,12 +12,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class ImageController {
+
+    private final UserRepository userRepository;
 
     @GetMapping ({"/", "/image/feed"})
     public String imageFeed (@AuthenticationPrincipal MyUserDetail userDetail, Model model) {
 
         log.info("username : {}", userDetail.getUsername());
+
+        User user = userRepository.findByUsername(userDetail.getUsername());
+
+        model.addAttribute("principalId", user.getId());
 
         return "image/feed";
     }
